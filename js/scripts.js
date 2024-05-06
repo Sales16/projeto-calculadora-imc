d75bc18168d857bee31bdaec8602c20ebf498610
Eduardo <eduardo.sales.silva1603@gmail.com>
2024-05-06 12:20:31 -0300

    Versao mobile melhorada

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
 const form = document.getElementById('form');
-
 form.addEventListener('submit', function(event){
     event.preventDefault();
 })
 
+let botaoCalcular = document.getElementById("calcular");
+botaoCalcular.addEventListener("click", calcularIMC);
+
+let botaoReset = document.getElementById("resetar");
+botaoReset.addEventListener("click", reset);
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === 'Enter') {
+        calcularIMC();
+    }
+});
+
 function calcularIMC() {
-    let altura = document.getElementById("altura").value;
+    let altura = document.getElementById("altura").value
     let peso = document.getElementById("peso").value;
     let resultado = document.getElementById("resultado");
 
-    let imc = (peso / (altura ** 2)).toFixed(2);
+    let imc = (peso / ((altura / 100) ** 2)).toFixed(2);
     resultado.textContent = 'Seu IMC é ' + imc;
     console.log(imc);
+}
+
+function reset() {
+    let resultado = document.getElementById("resultado");
+    resultado.textContent = '';    
 }
